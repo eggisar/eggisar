e4891c358ae4c6a17c6697b205e2bb272ec09e05
Monica Björk <m.bjork95@gmail.com>
2023-04-09 13:49:41 +0200

    added function to display date dynamicly
    not finished yet tho

--- frontend/src/components/ListElement.svelte
+++ frontend/src/components/ListElement.svelte
@@ -1,20 +1,36 @@
 <script lang="ts">
-    export let itemName = "";
+    export let itemName: string = "";
+    export let updateTime: string = "";
     export let value: any = undefined;
     
 	let selection: never[] = [];
-    let date = new Date().getHours() + ':' + new Date().getMinutes();
 
-    setTimeout(() => {
-        let checkboxElem = document.querySelector('.checkbox');
-        console.log(getComputedStyle(checkboxElem));
-    }, 3000);
+    const dateNow = new Date().toJSON();
+    let displayTime = updateTime.slice(0,10) + " " + updateTime.slice(11,16);
+    if(dateNow.slice(0,4) != updateTime.slice(0,4)) {
+        console.log("display full date");
+        displayTime = updateTime.slice(0,10) + " " + updateTime.slice(11,16);
+    } else if(dateNow.slice(5,7) != updateTime.slice(5,7)) {
+        console.log("display month and day");
+        displayTime = updateTime.slice(5,10) + " " + updateTime.slice(11,16);
+    } else if(dateNow.slice(8,10) != updateTime.slice(8,10)) {
+        console.log("display day");
+        displayTime = updateTime.slice(8,10) + " " + updateTime.slice(11,16);
+
+        // TODO: Add fancy functions to see 'X hours ago' or 'yesterday' or 'X days ago', maybe even update every minute?
+        // TODO: Update timezone?
+    }
+
+    // setTimeout(() => {
+    //     let checkboxElem = document.querySelector('.checkbox');
+    //     // console.log(getComputedStyle(checkboxElem));
+    // }, 3000);
 </script>
 
 <label class="wrapper">
     <input type="checkbox" bind:group={selection} value={value} class="checkbox" /> 
     {itemName}
-    <span class="date">{date}</span>
+    <span class="date">{displayTime}</span>
 </label>
 
 <style lang="scss">
