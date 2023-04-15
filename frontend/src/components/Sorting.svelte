be44ef826f283bd112d2306e5d3e591d4d81fc12
Monica Björk <m.bjork95@gmail.com>
2023-04-15 08:16:58 +0200

    Create Sorting.svelte

---
+++ frontend/src/components/Sorting.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+</script>
+
+<div class="wrapper">
+    <p> Sorting by.. </p>
+    <p>[icon]</p>
+</div>
+
+<style>
+    .wrapper {
+        margin: var(--margin-large) var(--margin-small);
+
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+        flex-direction: row;
+    }
+</style>
